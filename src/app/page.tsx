fa5ddf69126e6e2b676c1c4444d6b400ecd4349c
Hayden <hayden.hhoang@gmail.com>
2024-08-14 15:16:12 +0700

    add example search terms

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -82,7 +82,7 @@ export default function Home() {
           />
         </>
       ) : (
-        <ExampleSearchTerms />
+        <ExampleSearchTerms onClickAction={getCars} />
       )}
     </main>
   );
--- src/components/ExampleSearchTerms.tsx
+++ src/components/ExampleSearchTerms.tsx
@@ -1,12 +1,24 @@
 import { EXAMPLE_SEARCH_TERMS } from '@/utils/utils';
 
-export default function ExampleSearchTerms() {
+export default function ExampleSearchTerms({
+  onClickAction,
+}: {
+  onClickAction: (formData: FormData) => void;
+}) {
   return (
     <>
       <h2 className='w-full'>Try with these examples:</h2>
       <ul className='w-full flex flex-col gap-2 mt-2 text-sm font-light'>
         {EXAMPLE_SEARCH_TERMS.map((item) => (
-          <li className='w-full py-2.5 px-3 border rounded-sm cursor-pointer hover:bg-neutral-100 transition'>
+          <li
+            className='w-full py-2.5 px-3 border rounded-sm cursor-pointer hover:bg-neutral-100 transition'
+            onClick={async () => {
+              const formData = new FormData();
+              formData.append('q', item);
+              onClickAction(formData);
+            }}
+            key={item}
+          >
             {item}
           </li>
         ))}
