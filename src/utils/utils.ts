25853ff412c87d8e3b12e1d6573545e9d0eed9dd
Hayden <hayden.hhoang@gmail.com>
2024-08-15 10:18:49 +0700

    add more example searches

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -17,9 +17,15 @@ export const TRANSMISSION_TYPE = {
 };
 
 export const EXAMPLE_SEARCH_TERMS = [
-  'A honda or BMW with at least 200HP, rear wheel drive, from 20K to 50K, must be newer than 2014',
-  'At least 200hp, must not be of European car brands',
-  'Newest sedans, v6, above 300hp, best gas mileage',
+  'A honda or BMW with at least 200hp, rear wheel drive, from 20K to 50K, must be newer than 2014',
+  'At least 200hp, must be of American car brands',
+  'Newest sedans, V8, above 300hp, best gas mileage',
+  'Show me the most powerful car you have',
+  'Give me the latest cars',
+  'Diesel SUV, > 200hp',
+  "I don't know how to drive a manual",
+  'Electric cars under 30K',
+  'High performance Italian cars, above 700hp',
 ];
 
 export const TYPESENSE_PER_PAGE = 12;
