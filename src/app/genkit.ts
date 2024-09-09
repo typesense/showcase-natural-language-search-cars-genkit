176a853d870ebcf4125cd28b8019d546bbe227cd
Hayden <hayden.hhoang@gmail.com>
2024-09-09 20:31:13 +0700

    feat: dynamically read the collection schema to provide facet values for the llm

--- src/app/genkit.ts
+++ src/app/genkit.ts
@@ -8,6 +8,8 @@ import { dotprompt, defineDotprompt } from '@genkit-ai/dotprompt';
 import { _CarSchemaResponse, TypesenseQuerySchema } from '@/schemas/typesense';
 import { typesense } from '@/lib/typesense';
 
+const MAX_FACET_VALUES = Number(process.env.MAX_FACET_VALUES || '20');
+
 defineSchema('TypesenseQuery', TypesenseQuerySchema);
 
 configureGenkit({
@@ -16,6 +18,34 @@ configureGenkit({
   enableTracingAndMetrics: true,
 });
 
+// dynamically provide facet values for the llm
+async function getFieldEnumValues() {
+  const collection = await typesense.collections('cars').retrieve();
+  const facetableFields = collection.fields?.filter((field) => field.facet);
+
+  const facetValues = await typesense
+    .collections<_CarSchemaResponse>('cars')
+    .documents()
+    .search({
+      q: '*',
+      facet_by: facetableFields?.map(({ name }) => name).join(','),
+      max_facet_values: MAX_FACET_VALUES + 1, // plus 1 so we can check if any fields exceed the limit
+    });
+
+  return facetableFields
+    ?.map(({ type, name }, i) => {
+      const counts = facetValues.facet_counts?.[i].counts;
+      const exceedMaxNumValues =
+        counts && counts?.length > MAX_FACET_VALUES
+          ? 'There are more enum values for this field'
+          : 'N/A';
+      const enums = counts?.map((item) => item.value).join(', ');
+      // prettier-ignore
+      return `| ${name} | ${type} | Yes | No | ${enums} | ${exceedMaxNumValues} |`
+    })
+    .join('\n');
+}
+
 const generateTypesenseQuery = defineFlow(
   {
     name: 'generateTypesenseQuery',
@@ -23,20 +53,6 @@ const generateTypesenseQuery = defineFlow(
     outputSchema: TypesenseQuerySchema,
   },
   async (query) => {
-    const facetValues = await typesense
-      .collections<_CarSchemaResponse>('cars')
-      .documents()
-      .search({
-        q: '*',
-        facet_by: 'engine_fuel_type,market_category,vehicle_style',
-        max_facet_values: 20,
-      });
-    function getFacetEnums(index: number) {
-      return facetValues.facet_counts?.[index].counts
-        .map((item) => item.value)
-        .join(', ');
-    }
-    console.log(facetValues);
     const typesensePrompt = defineDotprompt(
       {
         model: 'googleai/gemini-1.5-flash',
@@ -97,24 +113,17 @@ Sorting hints:
 
 ## Car properties ##
 
-| Name              | Data Type | Filter | Sort | Enum Values                                                                                                 | Description|
-|-------------------|-----------|--------|------|-------------------------------------------------------------------------------------------------------------|------------|
-| make              | string    | Yes    | No   | N/A                                                                                                         | N/A        |
-| model             | string    | Yes    | No   | N/A                                                                                                         | N/A        |
-| year              | int64     | Yes    | Yes  | N/A                                                                                                         | N/A        |
-| engine_fuel_type  | string    | Yes    | No   | ${getFacetEnums(0)} | N/A        |
-| engine_hp         | float64   | Yes    | Yes  | N/A                                                                                                         | N/A        |
-| engine_cylinders  | int64     | Yes    | Yes  | N/A                                                                                                         | N/A        |
-| transmission_type | string    | Yes    | No   | MANUAL, AUTOMATIC, AUTOMATED_MANUAL, DIRECT_DRIVE                                                           | N/A        |
-| driven_wheels     | string    | Yes    | No   | rear wheel drive, front wheel drive, all wheel drive, four wheel drive                                      | N/A        |
-| market_category   | string    | Yes    | No   | ${getFacetEnums(1)} | N/A        |
-| number_of_doors   | int64     | Yes    | No   | N/A                                                                                                         | N/A        |
-| vehicle_size      | string    | Yes    | No   | Compact, Large, Midsize                                                                                     | N/A        |
-| vehicle_style     | string    | Yes    | No   | ${getFacetEnums(2)} | N/A        |
-| highway_mpg       | int64     | Yes    | Yes  | N/A                                                                                                         | N/A        |
-| city_mpg          | int64     | Yes    | Yes  | N/A                                                                                                         | N/A        |
-| popularity        | int64     | Yes    | Yes  | N/A                                                                                                         | N/A        |
-| msrp              | int64     | Yes    | Yes  | N/A                                                                                                         | in USD $   |
+| Name             | Data Type | Filter | Sort | Enum Values  | Description|
+|------------------|-----------|--------|------|--------------|------------|
+| year             | int64     | Yes    | Yes  | N/A          | N/A        |
+| engine_hp        | float64   | Yes    | Yes  | N/A          | N/A        |
+| engine_cylinders | int64     | Yes    | Yes  | N/A          | N/A        |
+| number_of_doors  | int64     | Yes    | No   | N/A          | N/A        |
+| highway_mpg      | int64     | Yes    | Yes  | N/A          | N/A        |
+| city_mpg         | int64     | Yes    | Yes  | N/A          | N/A        |
+| popularity       | int64     | Yes    | Yes  | N/A          | N/A        |
+| msrp             | int64     | Yes    | Yes  | N/A          | in USD $   |
+${await getFieldEnumValues()}
 
 ### IMPORTANT NOTES ###
  - Query Field: Include query only if both filter_by and sort_by are inadequate.
